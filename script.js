5b7a2710b5efa5de46e54f79f0496a2b430b7ac7
Keefe Custodio <keefecustodio@Keefes-MacBook-Air.local>
2021-06-30 19:48:46 -0700

    countdown timer functionality done

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const incrementBtn = document.getElementById('increment')
 const startBtn = document.getElementById('start')
 const timeSetDisplay = document.getElementById('time-set-display')
 
-let timeCounter = 0;
+let timeCounter = 0
+let start_stop = false
+let time_start
 
 decrementBtn.addEventListener('click', () => {
     if(timeCounter - 60 < 0) {
         timeCounter = 0;
     } else {
-        decrementCounter()
-        updateSetTimer()
+        decrementCounter()   
     }
-    
+    updateSetTimer()
 })
 
 incrementBtn.addEventListener('click', () => {
@@ -21,21 +22,42 @@ incrementBtn.addEventListener('click', () => {
 })
 
 startBtn.addEventListener('click', () => {
-    startTimer()
+    countdownStart()
 })
 
+/* Add Minutes */
 function incrementCounter() {
     timeCounter += 60
 }
 
+/* Subtract Minutes */
 function decrementCounter() {
     timeCounter -= 60
 }
 
+/* Updates timer after every decrement of timeCounter. */
 function updateSetTimer() {
-    timeSetDisplay.innerText = `${timeCounter / 60}:00`
+    timeSetDisplay.innerText = `${Math.floor(timeCounter / 60)}:${(timeCounter % 60) < 10 ? '0'+timeCounter % 60 : timeCounter % 60}`
 }
 
-function startTimer() {
-    let time_start = timeCounter
+function countdown() {
+    timeCounter--
+    if(timeCounter < 0) {
+        timeCounter = 0
+        clearInterval(time_start)
+    } else {
+        updateSetTimer()
+        console.log(timeCounter)
+    }
+}
+
+/* Start/Resumes/Stops the countdown */
+function countdownStart() {
+    if(!start_stop) {
+        start_stop = !start_stop
+        time_start = setInterval(() => {countdown()},1000)
+    } else {
+        clearInterval(time_start)
+        start_stop = !start_stop
+    }
 }
